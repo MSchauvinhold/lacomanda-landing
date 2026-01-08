5ce13fa4014de7b6f36931d488b4b6881bf11a76
Mateo Schauvinhold <Mateo_schauvinhold@hotmail.com>
2026-01-08 16:11:42 -0300

    numero whatsapp actualizado

--- src/App.tsx
+++ src/App.tsx
@@ -238,7 +238,7 @@ function App() {
 
   const sendToWhatsApp = (customerData: CustomerData) => {
     const message = buildWhatsAppMessage(customerData);
-    const whatsappNumber = '5493772406996';
+    const whatsappNumber = '5493772300876';
     const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${message}`;
     
     window.open(whatsappUrl, '_blank');
